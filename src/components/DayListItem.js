bc48d660e78b9d713694602e1ef7ccfa3db8540d
John O'Halloran <john.ohalloran@telus.com>
2023-01-10 11:39:25 -0500

    add dayListItems and unit test

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+  
+  const formatSpots = input => {
+    let output = `${input} spots remaining`
+
+    input === 1 && (output = `${input} spot remaining`);
+    input <= 0 && (output = `no spots remaining`);
+
+    return output
+  }
+
+  const daysLeftText = formatSpots(props.spots)
+
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{daysLeftText}</h3>
+    </li>
+  );
+}
