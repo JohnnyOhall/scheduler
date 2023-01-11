26b4d19411fcd0411005a69e8ba82badfa97984e
John O'Halloran <john.ohalloran@telus.com>
2023-01-10 19:07:46 -0500

    story works for interviewList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,13 +5,25 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
 
+  const interviewerItem = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewerItem}</ul>
     </section>
   );
-}
+};
 
 
 
