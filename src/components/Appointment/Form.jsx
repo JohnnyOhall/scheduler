d34eac84389c0ba4ce4db15d6d7b917eafef11eb
John O'Halloran <john.ohalloran@telus.com>
2023-01-12 10:52:22 -0500

    add component to index and add stories

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+// import "components/Appointment/**STYLE**.scss"; <---- uncomment this if style needed
+
+export default function Form(props) {
+  const { student, interviewer, interviewers, onSave, onCancel } = props,
+    [ studentName, setStudentName ] = useState( student || '' ),
+      [ interviewerName, setInterviewerName ] = useState( interviewer || null );
+
+  const reset = () => {
+    setStudentName( '' )
+    setInterviewerName( null )
+  };
+
+  const cancel = () => {
+    reset()
+    onCancel()
+  };
+
+  const save = () => onSave( studentName, interviewerName );
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={ e => e.preventDefault() }>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={ studentName }
+            onChange={ e => setStudentName( e.target.value ) }
+          />
+        </form>
+        <InterviewerList
+          interviewers={ interviewers }
+          value={ interviewerName }
+          onChange={ setInterviewerName }       
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={ cancel }>Cancel</Button>
+          <Button confirm onClick={ save }>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment( props ) {
+  const { time, interview } = props;
 
+  const EmptyOrShow = interview ?
+    <Show student={interview.student} interviewer={interview.interviewer} /> :
+    <Empty />;
+  
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time} />
+      {EmptyOrShow}
+    </article>
   );
 }
